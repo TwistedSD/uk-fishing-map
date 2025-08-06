107009ae524240ac8a15581e1a73b807424b4ffb
TwistedSD <38792636+TwistedSD@users.noreply.github.com>
2025-08-06 22:58:34 +0100

    Add files via upload

--- netlify/functions/count.js
+++ netlify/functions/count.js
@@ -1,20 +1,23 @@
 import { getStore } from "@netlify/blobs";
 
 export const handler = async (event, context) => {
-    try {
-        const store = getStore("visits");
-        let visitCount = await store.get("count", { type: "json" }) || { count: 0 };
-        visitCount.count += 1;
-        await store.set("count", JSON.stringify(visitCount));
+  const store = getStore("visits");
+  let visitCount;
 
-        return {
-            statusCode: 200,
-            body: JSON.stringify(visitCount),
-        };
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: 'Failed to update visit count.' }),
-        };
-    }
+  try {
+    visitCount = await store.get("count", { type: "json" }) || { count: 0 };
+    visitCount.count += 1;
+    await store.set("count", JSON.stringify(visitCount));
+  } catch (error) {
+    console.error("Failed to update visit count:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to update visit count.' }),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(visitCount),
+  };
 };
